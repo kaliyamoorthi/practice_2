63f3f054fee6f8a4ced6211a14cbf7b67d7a3204
niitagile <43024343+niitagile@users.noreply.github.com>
2022-07-27 14:59:41 +0530

    Add files via upload

---
+++ July27/innerclasses/innerClassAssisted2.java
@@ -0,0 +1,26 @@
+package innerclasses;
+public class innerClassAssisted2 {
+
+private String msg="Inner Classes";
+
+ void display(){  
+	 class Inner{  
+		 void msg(){
+			 System.out.println(msg);
+		 }  
+  }  
+  
+  Inner l=new Inner();  
+  l.msg();  
+ }//display end  
+
+ 
+public static void main(String[] args) {
+	innerClassAssisted2  ob=new innerClassAssisted2 ();  
+	ob.display();  
+	}
+}
+
+
+
+	
---
+++ July27/innerclasses/innerClassAssisted3.java
@@ -0,0 +1,29 @@
+package innerclasses;
+//anonymous inner class
+abstract class AnonymousInnerClass {
+	   public abstract void display();
+	   void show(){
+		System.out.println("show method");   
+	   }
+	}
+
+
+public class innerClassAssisted3 {
+
+	public static void main(String[] args) {
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
+
+
+
+
+
+
+
